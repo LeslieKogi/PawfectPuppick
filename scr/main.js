58cec578aebf65f9111d330be2a4f81a69f0e91c
Leslie Kogi <lesliekogi@gmail.com>
2025-02-05 21:55:56 +0300

    validation of form input

--- scr/main.js
+++ scr/main.js
@@ -1,5 +1,19 @@
 document.addEventListener ("DOMContentLoaded",function(){
-     document.getElementById("Perfectpuppyform").addEventListener('submot')
+     document.getElementById("Perfectpuppyform").addEventListener('submit',function(event)//event listener to listen to the submit form
+     {event.preventDefault()
+     userinput()}
+    )
+})
+function userinput(){
+    const dogtype = document.getElementById( "dogtype").value;
+    const Areatype = document.getElementById("Areatype").value;
+    const timetype = document.getElementById("timetype").value;
+    const plantype = document.getElementById("plantype").value;
+
+    console.log(dogtype,Areatype,timetype,plantype, )
+    if(!dogtype || ! Areatype || !timetype || plantype){
+        alert("Please fill out details ")
+        return
+    }
 }
 
-)
