b790d4d29431380f0644a4d0d866d84c47b1f0fb
Leslie Kogi <lesliekogi@gmail.com>
2025-02-06 14:04:40 +0300

    js logic done

--- scr/main.js
+++ scr/main.js
@@ -6,14 +6,58 @@ document.addEventListener ("DOMContentLoaded",function(){
 })
 function userinput(){
     const dogtype = document.getElementById( "dogtype").value;
-    const Areatype = document.getElementById("Areatype").value;
+    const areatype = document.getElementById("areatype").value;
     const timetype = document.getElementById("timetype").value;
     const plantype = document.getElementById("plantype").value;
 
-    console.log(dogtype,Areatype,timetype,plantype, )
-    if(!dogtype || ! Areatype || !timetype || plantype){
-        alert("Please fill out details ")
+    console.log(dogtype,areatype,timetype,plantype, )
+    if(!dogtype || ! areatype || !timetype || !plantype){
+        alert("Please fill out details ") //ensures the form is filled
         return
     }
+
+//Determination of breeds 
+
+let possibleBreeds=[];
+
+//based on gaurd/house dog preference
+
+if(dogtype==="Gaurd dog"){
+    possibleBreeds = ["German Shepherd", "Doberman", "Rottweiler", "Great Dane"];
+} else if (dogtype==="house dog")
+{
+    possibleBreeds = ["Labrador Retriever", "Beagle", "Poodle", "Pug"];
+}
+
+//based on area type
+possibleBreeds=possibleBreeds.filter(breed=>{
+    if(areatype=="hot"){
+        return ["Rottweiler", "Doberman", "Labrador Retriever", "Beagle"].includes(breed);
+
+    }else if(Areatype=="cold"){
+        return ["German Shepherd", "Great Dane", "Poodle", "Pug"].includes(breed);
+    }
+    return false;
+});
+
+// based on frequennt vet visits
+if (timetype==="No"){
+    possibleBreeds = possibleBreeds.filter(breed => !["Poodle", "Great Dane"].includes(breed));
+}
+
+//based on exercise needs
+if(plantype==="No"){
+    possibleBreeds=possibleBreeds.filter(breed=>!["German Shepherd","Labrador Retriever"].includes(breed));
+}
+
+//Display the results
+
+if(possibleBreeds.length>0){
+    alert("Your ideal dog breed(s):"+possibleBreeds.join(","));
+
+}else{
+    alert("No breed matches.Try adjusting your preferences.");
+}
+
 }
 
